2671907b0e7c42b30bf556d1aca50908248009e8
Maicon <153109959+mbnunes2023@users.noreply.github.com>
2025-09-26 09:46:05 -0300

    Update page.tsx

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useMemo, useRef, useState } from "react";
-import { supabase } from "../../lib/supabaseClient";
+import { supabase } from "../../../lib/supabaseClient";
 import ExportPDFButton from "../components/ExportPDFButton";
 
 import {
